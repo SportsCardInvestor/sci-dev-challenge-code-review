fa4f1d3d411bc1cdcdc1a27a9554ccd6d77b14cf
Dondrey Taylor <dondrey@solofunds.com>
2025-06-01 10:01:06 -0400

    Updated cdk app to include account and region to fix mixmatch error on deploy

--- bin/mtg-wizards-api.ts
+++ bin/mtg-wizards-api.ts
@@ -3,4 +3,9 @@ import * as cdk from 'aws-cdk-lib';
 import { MtgWizardsApiStack } from '../lib/mtg-wizards-api-stack';
 
 const app = new cdk.App();
-new MtgWizardsApiStack(app, 'MtgWizardsApiStack');
+new MtgWizardsApiStack(app, 'MtgWizardsApiStack', {
+    env: {
+        account: process.env.AWS_ACCOUNT_ID,
+        region: process.env.AWS_REGION,
+    }
+});
